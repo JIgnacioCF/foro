9fa7e0260d43cb05b4cf612a6a7060a0f3606374
JIgnacioCF <111895507+JIgnacioCF@users.noreply.github.com>
2022-09-01 14:46:40 -0500

    Avance 010922

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: string = 'admin';
+  pass: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  entrar() {
+    alert('yupiiiii' + this.user + " " + this.pass);
+  }
 }
